45ff6d1cc5bddb010c6332f6b7f8f8c82085b23c
sidharth00007 <championsid07@gmail.com>
2023-08-08 22:09:21 +0530

    updated redirect uri

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       domain="dev-weu524424ffwvvb7.us.auth0.com"
       clientId="OARRTVtkMrc03tBdU6Ozyfo6kQ0ejzNK"
       authorizationParams={{
-        redirect_uri: "http://localhost:5173"
+        redirect_uri: "https://homyz-zeta.vercel.app"
        }}
       audience="http://localhost:8000"
       scope="openid profile email"
