dbe9a26fb975e61bfb9a845044068793131e0cc4
sidharth00007 <championsid07@gmail.com>
2023-08-10 13:30:36 +0530

    changed searchbar

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -28,7 +28,7 @@ const Hero = () => {
             <span>Forget all difficulties in finding a residence for you</span>
           </div>
 
-          <SearchBar />
+          {/* <SearchBar /> */}
           <div className="flexCenter stats">
             <div className="flexColCenter stat">
               <span>
